720bbd402a90ac8bdbce33676fd857c4fc93b109
Alex <48862843+LobanAB@users.noreply.github.com>
2021-08-23 20:28:29 +0300

    add run_fix.py

--- run.py
+++ run.py
@@ -11,8 +11,8 @@ def show_frame(frame):
     cv2.imshow('Video', frame)
 
 
-def draw_sqare(frame, color):
-        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+def draw_square(frame, color):
+    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
 
 
 def get_cascades():
@@ -41,7 +41,7 @@ if __name__ == "__main__":
             )]
             for capture in captures:
                 for (x, y, w, h) in capture:
-                    draw_sqare(webcam_frame, color)
+                    draw_square(webcam_frame, color)
         show_frame(webcam_frame)
 
         if is_user_wants_quit():
